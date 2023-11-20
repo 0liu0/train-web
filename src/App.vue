2c2464fa47f906ebdd20d8750a8977ac33d615b5
liuche <706716852@qq.com>
2023-11-20 13:18:42 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <a-button>nihaoa</a-button>
+  <RadiusBottomrightOutlined />
+<!--  <router-view/>-->
 </template>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Antd from 'ant-design-vue';
+import * as Icons from '@ant-design/icons-vue';
+import 'ant-design-vue/dist/reset.css';
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+// 全局使用图标
+const icons = Icons;
+for (const i in icons){
+    app.component(i, icons[i])
+}
+app.use(Antd).use(router).use(store).mount('#app');
+console.log("环境：",process.env.NODE_ENV);
+console.log("服务端：",process.env.VUE_APP_SERVER);
