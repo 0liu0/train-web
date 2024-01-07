602c15f5063315aece9928b807493d91e8126186
liuche <706716852@qq.com>
2024-01-07 11:17:55 +0800

    修改页面图标

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,42 +1,45 @@
 <template>
-  <a-row class="login">
-    <a-col :span="8" :offset="8" class="login-main">
-      <h1 style="text-align: center">
-        <rocket-two-tone/>&nbsp;刘彻12306售票系统
-      </h1>
-      <a-form
-          :model="loginForm"
-          name="basic"
-          autocomplete="off"
-      >
-        <a-form-item
-            label=""
-            name="mobile"
-            :rules="[{ required: true, message: '请输入手机号!' }]"
+  <div class="bg">
+    <img src="../assets/img.png" alt="nihao" style="width: auto; height: 100%; display: block;">
+    <a-row class="login">
+      <a-col :span="8" :offset="8" class="login-main">
+        <h1 style="text-align: center">
+          <rocket-two-tone/>&nbsp;刘彻12306贩票系统
+        </h1>
+        <a-form
+            :model="loginForm"
+            name="basic"
+            autocomplete="off"
         >
-          <a-input v-model:value="loginForm.mail" placeholder="手机号"/>
-        </a-form-item>
+          <a-form-item
+              label=""
+              name="mobile"
+              :rules="[{ required: true, message: '请输入手机号!' }]"
+          >
+            <a-input v-model:value="loginForm.mail" placeholder="手机号"/>
+          </a-form-item>
 
-        <a-form-item
-            label=""
-            name="code"
-            :rules="[{ required: true, message: '请输入验证码!' }]"
-        >
-          <a-input v-model:value="loginForm.mailCode">
-            <template #addonAfter>
-              <a @click="sendCode">获取验证码</a>
-            </template>
-          </a-input>
-          <!--<a-input v-model:value="loginForm.code" placeholder="验证码"/>-->
-        </a-form-item>
+          <a-form-item
+              label=""
+              name="code"
+              :rules="[{ required: true, message: '请输入验证码!' }]"
+          >
+            <a-input v-model:value="loginForm.mailCode">
+              <template #addonAfter>
+                <a @click="sendCode">获取验证码</a>
+              </template>
+            </a-input>
+            <!--<a-input v-model:value="loginForm.code" placeholder="验证码"/>-->
+          </a-form-item>
 
-        <a-form-item>
-          <a-button type="primary" block @click="login">登录</a-button>
-        </a-form-item>
+          <a-form-item>
+            <a-button type="primary" block @click="login">登录</a-button>
+          </a-form-item>
 
-      </a-form>
-    </a-col>
-  </a-row>
+        </a-form>
+      </a-col>
+    </a-row>
+  </div>
 </template>
 
 <script setup>
@@ -88,8 +91,17 @@ const login = () => {
 .login-main {
   margin-top: 100px;
   padding: 30px 30px 20px;
-  border: 2px solid grey;
+  border: 2px solid white;
   border-radius: 10px;
   background-color: #fcfcfc;
+}
+
+
+img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1000; /* or any negative value */
+  /* ... other styles */
 }
 </style>
