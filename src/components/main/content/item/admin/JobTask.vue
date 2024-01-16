fcab3b10dd13a1da8ba0e93287adefbf4251970f
刘祖彻 <706716852@qq.com>
2024-01-16 23:55:10 +0800

    优化定时任务界面

--- src/components/main/content/item/admin/JobTask.vue
+++ src/components/main/content/item/admin/JobTask.vue
@@ -1,61 +1,43 @@
 <template>
   <div class="job">
     <p>
-      <a-button type="primary" @click="handleAdd()">
-        新增
-      </a-button>&nbsp;
-      <a-button type="primary" @click="handleQuery()">
-        刷新
-      </a-button>
+      <a-button type="primary" @click="handleAdd()">新增</a-button>&nbsp;
+      <a-button type="primary" @click="handleQuery()">刷新</a-button>
     </p>
-    <a-table :dataSource="jobs"
-             :columns="columns"
-             :loading="loading">
+    <a-table :dataSource="jobs" :columns="columns" :loading="loading">
       <template #bodyCell="{ column, record }">
         <template v-if="column.dataIndex === 'operation'">
           <a-space>
             <a-popconfirm
-                title="手动执行会立即执行一次，确定执行？"
-                ok-text="是"
-                cancel-text="否"
-                @confirm="handleRun(record)"
+              title="手动执行会立即执行一次，确定执行？"
+              ok-text="是"
+              cancel-text="否"
+              @confirm="handleRun(record)"
             >
-              <a-button type="primary" size="small">
-                手动执行
-              </a-button>
+              <a-button type="primary" size="small">手动执行</a-button>
             </a-popconfirm>
-            <a-popconfirm
-                title="确定重启？"
-                ok-text="是"
-                cancel-text="否"
-                @confirm="handleResume(record)"
-            >
-              <a-button v-show="record.state === 'PAUSED' || record.state === 'ERROR'" type="primary" size="small">
-                重启
-              </a-button>
+            <a-popconfirm title="确定重启？" ok-text="是" cancel-text="否" @confirm="handleResume(record)">
+              <a-button
+                v-show="record.state === 'PAUSED' || record.state === 'ERROR'"
+                type="primary"
+                size="small"
+              >重启</a-button>
             </a-popconfirm>
-            <a-popconfirm
-                title="确定暂停？"
-                ok-text="是"
-                cancel-text="否"
-                @confirm="handlePause(record)"
-            >
-              <a-button v-show="record.state === 'NORMAL' || record.state === 'BLOCKED'" type="primary" size="small">
-                暂停
-              </a-button>
+            <a-popconfirm title="确定暂停？" ok-text="是" cancel-text="否" @confirm="handlePause(record)">
+              <a-button
+                v-show="record.state === 'NORMAL' || record.state === 'BLOCKED'"
+                type="primary"
+                size="small"
+              >暂停</a-button>
             </a-popconfirm>
-            <a-button type="primary" @click="handleEdit(record)" size="small">
-              编辑
-            </a-button>
+            <a-button type="primary" @click="handleEdit(record)" size="small">编辑</a-button>
             <a-popconfirm
-                title="删除后不可恢复，确认删除?"
-                ok-text="是"
-                cancel-text="否"
-                @confirm="handleDelete(record)"
+              title="删除后不可恢复，确认删除?"
+              ok-text="是"
+              cancel-text="否"
+              @confirm="handleDelete(record)"
             >
-              <a-button type="danger" size="small">
-                删除
-              </a-button>
+              <a-button type="danger" size="small">删除</a-button>
             </a-popconfirm>
           </a-space>
         </template>
@@ -63,27 +45,32 @@
     </a-table>
 
     <a-modal
-        title="用户"
-        v-model:visible="modalVisible"
-        :confirm-loading="modalLoading"
-        @ok="handleModalOk"
+      title="新增定时任务"
+      v-model:visible="modalVisible"
+      :confirm-loading="modalLoading"
+      @ok="handleModalOk"
     >
       <a-form :model="job" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
-        <a-form-item label="类名">
-          <a-input v-model:value="job.name" />
-        </a-form-item>
-        <a-form-item label="描述">
-          <a-input v-model:value="job.description" />
+        <a-form-item label="选择任务">
+          <!-- <a-input v-model:value="job.name" @click="getJobInfo" /> -->
+          <a-select
+            v-model:value="job.name"
+            show-search
+            placeholder="选择一个定时任务"
+            style="width: 200px"
+            :options="jobList"
+            @change="handleChange"
+            @click="getJobInfo"
+          ></a-select>
         </a-form-item>
         <a-form-item label="分组">
-          <a-input v-model:value="job.group" :disabled="!!job.state"/>
+          <a-input v-model:value="job.group" :disabled="!!job.state" />
         </a-form-item>
         <a-form-item label="表达式">
           <a-input v-model:value="job.cronExpression" />
           <div class="ant-alert ant-alert-success">
             每5秒执行一次：0/5 * * * * ?
-            <br>
-            每5分钟执行一次：* 0/5 * * * ?
+            <br />每5分钟执行一次：* 0/5 * * * ?
           </div>
         </a-form-item>
       </a-form>
@@ -92,53 +79,63 @@
 </template>
 
 <script>
-import { defineComponent, onMounted, ref } from 'vue';
-import { notification } from 'ant-design-vue';
+import { defineComponent, onMounted, ref } from "vue";
+import { notification } from "ant-design-vue";
 import myAxios from "@/utils/myAxios";
-import {Tool} from "@/utils/tool";
+import { Tool } from "@/utils/tool";
 
 export default defineComponent({
-  name: 'batch-job-view',
-  setup () {
+  name: "batch-job-view",
+  setup() {
     const jobs = ref();
     const loading = ref();
 
-    const columns = [{
-      title: '分组',
-      dataIndex: 'group',
-    }, {
-      title: '类名',
-      dataIndex: 'name',
-    }, {
-      title: '描述',
-      dataIndex: 'description',
-    }, {
-      title: '状态',
-      dataIndex: 'state',
-    }, {
-      title: '表达式',
-      dataIndex: 'cronExpression',
-    }, {
-      title: '上次时间',
-      dataIndex: 'preFireTime',
-    }, {
-      title: '下次时间',
-      dataIndex: 'nextFireTime',
-    }, {
-      title: '操作',
-      dataIndex: 'operation'
-    }];
-
+    const columns = [
+      {
+        title: "分组",
+        dataIndex: "group"
+      },
+      {
+        title: "类名",
+        dataIndex: "name"
+      },
+      {
+        title: "描述",
+        dataIndex: "description"
+      },
+      {
+        title: "状态",
+        dataIndex: "state"
+      },
+      {
+        title: "表达式",
+        dataIndex: "cronExpression"
+      },
+      {
+        title: "上次时间",
+        dataIndex: "preFireTime"
+      },
+      {
+        title: "下次时间",
+        dataIndex: "nextFireTime"
+      },
+      {
+        title: "操作",
+        dataIndex: "operation"
+      }
+    ];
+    const jobList = ref([]);
+    const jobDataInfo = ref();
     const handleQuery = () => {
       loading.value = true;
       jobs.value = [];
-      myAxios.get('/batch/admin/job/query').then((response) => {
+      myAxios.get("/batch/admin/job/query").then(response => {
         loading.value = false;
         const data = response.data;
         if (data.success) {
           jobs.value = data.content;
         } else {
-          notification.error({description: data.message});
+          notification.error({ description: data.message });
         }
       });
     };
@@ -154,15 +151,16 @@ export default defineComponent({
       if (job.value.state) {
         url = "reschedule";
       }
-      myAxios.post('/batch/admin/job/' + url, job.value).then((response) => {
+      console.log("job::", job.value);
+      myAxios.post("/batch/admin/job/" + url, job.value).then(response => {
         modalLoading.value = false;
         const data = response.data;
         if (data.success) {
           modalVisible.value = false;
-          notification.success({description: "保存成功！"});
+          notification.success({ description: "保存成功！" });
           handleQuery();
         } else {
-          notification.error({description: data.message});
+          notification.error({ description: data.message });
         }
       });
     };
@@ -173,14 +171,14 @@ export default defineComponent({
     const handleAdd = () => {
       modalVisible.value = true;
       job.value = {
-        group: 'DEFAULT'
+        group: "DEFAULT"
       };
     };
 
     /**
      * 编辑
      */
-    const handleEdit = (record) => {
+    const handleEdit = record => {
       modalVisible.value = true;
       job.value = Tool.copy(record);
     };
@@ -188,52 +186,56 @@ export default defineComponent({
     /**
      * 删除
      */
-    const handleDelete = (record) => {
-      myAxios.post('/batch/admin/job/delete', {
-        name: record.name,
-        group: record.group
-      }).then((response) => {
-        const data = response.data;
-        if (data.success) {
-          notification.success({description: "删除成功！"});
-          handleQuery();
-        } else {
-          notification.error({description: data.message});
-        }
-      });
+    const handleDelete = record => {
+      myAxios
+        .post("/batch/admin/job/delete", {
+          name: record.name,
+          group: record.group
+        })
+        .then(response => {
+          const data = response.data;
+          if (data.success) {
+            notification.success({ description: "删除成功！" });
+            handleQuery();
+          } else {
+            notification.error({ description: data.message });
+          }
+        });
     };
 
     /**
      * 暂停
      */
-    const handlePause = (record) => {
-      myAxios.post('/batch/admin/job/pause', {
-        name: record.name,
-        group: record.group
-      }).then((response) => {
-        const data = response.data;
-        if (data.success) {
-          notification.success({description: "暂停成功！"});
-          handleQuery();
-        } else {
-          notification.error({description: data.message});
-        }
-      });
+    const handlePause = record => {
+      myAxios
+        .post("/batch/admin/job/pause", {
+          name: record.name,
+          group: record.group
+        })
+        .then(response => {
+          const data = response.data;
+          if (data.success) {
+            notification.success({ description: "暂停成功！" });
+            handleQuery();
+          } else {
+            notification.error({ description: data.message });
+          }
+        });
     };
 
     /**
      * 重启
      */
-    const handleResume = (record) => {
-      myAxios.post('/batch/admin/job/reschedule', record).then((response) => {
+    const handleResume = record => {
+      myAxios.post("/batch/admin/job/reschedule", record).then(response => {
         modalLoading.value = false;
         const data = response.data;
         if (data.success) {
           modalVisible.value = false;
-          notification.success({description: "重启成功！"});
+          notification.success({ description: "重启成功！" });
           handleQuery();
         } else {
-          notification.error({description: data.message});
+          notification.error({ description: data.message });
         }
       });
     };
@@ -241,13 +243,13 @@ export default defineComponent({
     /**
      * 手动执行
      */
-    const handleRun = (record) => {
-      myAxios.post('/batch/admin/job/run', record).then((response) => {
+    const handleRun = record => {
+      myAxios.post("/batch/admin/job/run", record).then(response => {
         const data = response.data;
         if (data.success) {
-          notification.success({description: "手动执行成功！"});
+          notification.success({ description: "手动执行成功！" });
         } else {
-          notification.error({description: data.message});
+          notification.error({ description: data.message });
         }
       });
     };
@@ -256,8 +258,42 @@ export default defineComponent({
       return Tool.getEnumValue(key, obj);
     };
 
+    const getJobInfo = () => {
+      myAxios.get("/batch/admin/job/get/job-desc").then(resp => {
+        const data = resp.data;
+        if (data.success) {
+          // 获取content
+          // const contentObject = jsonObject.content;
+          let data = resp.data.content;
+          jobDataInfo.value = data
+          jobList.value = [];
+          for (const key in jobDataInfo.value) {
+            if (data.hasOwnProperty(key)) {
+              const value = jobDataInfo.value[key];
+              console.log(`Key: ${key}, Value: ${value}`);
+              // 在这里可以对每个键值对进行相应的操作
+              let option = {
+                label: "",
+                value: ""
+              };
+              option.label = value;
+              option.value = key;
+              jobList.value.push(option);
+            }
+          }
+          console.log("jobList::", jobList.value);
+        } else {
+          notification.error({ description: data.message });
+        }
+      });
+    };
+
+    const handleChange = value => {
+      console.log(`selected ${value}`);
+      job.value.description = jobDataInfo.value[value]
+    };
     onMounted(() => {
-      console.log('index mounted!');
+      console.log("index mounted!");
       handleQuery();
     });
 
@@ -266,6 +302,8 @@ export default defineComponent({
       jobs,
       loading,
       handleQuery,
+      jobList,
+      jobDataInfo,
 
       handleAdd,
       handleEdit,
@@ -277,10 +315,12 @@ export default defineComponent({
       modalLoading,
       handleModalOk,
       getEnumValue,
-      handleRun
+      handleRun,
+      getJobInfo,
+      handleChange
     };
   }
-})
+});
 </script>
 
 <style scoped>
